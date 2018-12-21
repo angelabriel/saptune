2f4e254610120df69eef6fdc3ee7bb29ad517196
AngelaBriel <abriel@suse.com>
2018-12-21 17:54:08 +0100

    add tests and docker for testing

--- run_travis_tst.sh
+++ run_travis_tst.sh
@@ -7,8 +7,8 @@ env
 
 echo "zypper in ..."
 zypper -n --gpg-auto-import-keys ref && zypper -n --gpg-auto-import-keys in systemd
-systemctl start dbus
-zypper -n --gpg-auto-import-keys ref && zypper -n --gpg-auto-import-keys in go1.7 go cpupower uuidd git
+/bin/systemctl start dbus
+zypper -n --gpg-auto-import-keys ref && zypper -n --gpg-auto-import-keys in go1.7 go cpupower uuidd git tuned
 
 echo "PATH is $PATH, GOPATH is $GOPATH, TRAVIS_HOME is $TRAVIS_HOME"
 
@@ -25,22 +25,14 @@ ln -s /app/testdata/saptune-test-solutions /usr/share/saptune/solutions
 
 # ANGI TODO - remove later
 echo "check system environment for tests"
-pwd
-ls -al
-echo "available system parameter:"
-sysctl -A
 echo "check cpu governer"
-ls /sys/devices/system/cpu/cpu0/cpufreq
-cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors
-echo "check /dev/shm"
-mount | grep /dev/shm
-df -h /dev/shm
-df /dev/shm
+ls /sys/devices/system/cpu/
+ls /sys/devices/system/cpu/cpu0/
 echo "services"
-systemctl
+type systemctl
+/bin/systemctl start dbus
+/bin/systemctl
 ls /etc/systemd
-ls /etc/systemd/logind.conf.d
-cat /etc/systemd/logind.conf.d/sap.conf
 
 echo "go environment:"
 go env
@@ -55,5 +47,5 @@ go test -v -cover ./...
 go build
 echo "run goveralls"
 $GOPATH/bin/goveralls -service=travis-ci
-ls -l /root/.coverprofile
+#ls -l /root/.coverprofile
 
